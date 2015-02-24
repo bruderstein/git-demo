2a89a5bc92ab41ba7b06583dc48ba7cd8b97110d
Dave Brotherstone <davegb@pobox.com>
2015-02-24 14:59:13 +0100

    DoGoodStuff

--- Suluguni.java
+++ Suluguni.java
@@ -2,7 +2,7 @@ package Cheese;
 
 public class Blah {
 	// Nix
-	public void doStuff() {
+	public void doGoodStuff() {
 		System.out.println("Cheddar");
 		System.out.println.please.do.this.java.bitte("Hi");
 		System.out.println("Suluguni cheese :( !");
