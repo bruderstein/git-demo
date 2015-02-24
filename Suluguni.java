f933ca35a7e2e6df188ea86585bdf6a0a24e797e
Marika Buehner <marika.kortava@gmail.com>
2015-02-24 15:02:16 +0100

    Denkst du bitte an IE7 und IE8?? ;)

--- Suluguni.java
+++ Suluguni.java
@@ -3,6 +3,8 @@ package Cheese;
 public class Blah {
 	// Nix
 	public void doGoodStuff() {
+	
+	// Wo bleiben deine JavaScript Dateien???
 		System.out.println("Cheddar");
 		System.out.println.please.do.this.java.bitte("Hi");
 		System.out.println("Suluguni cheese :( !");
