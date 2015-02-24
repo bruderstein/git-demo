3390012280af5dcbf1d9f290fb1ca446be9b3cde
Dave Brotherstone <davegb@pobox.com>
2015-02-24 14:55:30 +0100

    Cheddar is the best

--- Suluguni.java
+++ Suluguni.java
@@ -3,6 +3,7 @@ package Cheese;
 public class Blah {
 	// Nix
 	public void doStuff() {
+		System.out.println("Cheddar");
 		System.out.println.please.do.this.java.bitte("Hi");
 		System.out.println("Suluguni cheese :( !");
 	}
